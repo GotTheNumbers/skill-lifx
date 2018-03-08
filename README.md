73613fded0d2d303e4ed5959aa42d9475fe8f165
GotTheNumbers <iamanthony@gmail.com>
2018-03-07 21:36:52 -0500

    Many changes to intent parsing

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-To submit your skill, replace this file with text from 
-https://rawgit.com/MycroftAI/mycroft-skills/master/meta_editor.html
+## lifx-skill
+Control your Lifx lights with Mycroft.
 
+## Description
+A <a href='https://mycroft.ai/'>Mycroft</a> skill for controlling <a href='https://www.lifx.com/'>Lifx</a> lights.
 
-## YOUR SKILL NAME
-One line description of the skill
+## Examples
 
-## Description 
-A more verbose description, including any extra instructions or
-information that didn't fit in the one line.
-
-## Examples 
-* "Hello world"
-* "Greetings planet earth"
-* "Count up"
-* "Count down"
-
-## Credits 
-Your name
+## Credits
+Anthony DiGiovanni
--- __init__.py
+++ __init__.py
@@ -15,27 +15,59 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import json
+import sys
 
 from adapt.intent import IntentBuilder
+from adapt.engine import IntentDeterminationEngine
 from mycroft.skills.core import MycroftSkill, intent_handler
-from mycroft.util.log import LOG
+from mycroft.util.log import LOG, getLogger
 
 from pifx import PIFX
 
 __author__ = 'GotTheNumbers'
 
+#start logging
+LOGGER = getLogger(__name__)
+
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
-
 class LifxSkill(MycroftSkill):
-
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(LifxSkill, self).__init__(name="LifxSkill")
+        self.register_intent_file('play.intent', self.handle_play_intent)
+
+    engine = IntentDeterminationEngine()
+
+    #define lifx
+    lifx_keyword = ["turn"]
+
+    for lk in lifx_keyword:
+        engine.register_entity(lk, "LifxKeyword")
+
+    lifx_verbs = ["on", "off"]
+
+    for lv in lifx_verbs:
+        engine.register_entity(lv, "LifxVerb")
+
+    device_names = ["kitchen", "lamp"]
+
+    for dv in device_names:
+        engine.register_entity(dv, "DeviceName")
+
+    lifx_intent = IntentBuilder("LifxIntent")\
+        .require("LifxKeyword")\
+        .require("LifxVerb")\
+        .require("DeviceName")\
+        .build()
+
+    engine.register_intent_parser(lifx_intent)
 
     # The ON intent handler
     @intent_handler(IntentBuilder("OnIntent").require("OnKeyword"))
     def handle_on_intent(self, message):
+        self.speak(str(message.data))
         if len(self.settings["Token"]) == 0:
             self.speak_dialog("please.add.access.token")
             return
@@ -53,14 +85,9 @@ class LifxSkill(MycroftSkill):
         p.set_state(power="off")
         self.speak_dialog("off")
 
-    # The "stop" method defines what Mycroft does when told to stop during
-    # the skill's execution. In this case, since the skill's functionality
-    # is extremely simple, there is no need to override it.  If you DO
-    # need to implement stop, you should return True to indicate you handled
-    # it.
-    #
-    # def stop(self):
-    #    return False
+    # The "stop" method defines what Mycroft does when told to stop
+    def stop(self):
+        return True
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
